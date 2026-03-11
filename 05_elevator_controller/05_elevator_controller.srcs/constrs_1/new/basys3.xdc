fd20333d0fda40fffa3fd28b1e33e1948367ba41
kylianchaussoy <kyliannchaussoy@gmail.com>
2026-03-10 20:00:48 -0400

    naming uniformity

--- 05_elevator_controller/05_elevator_controller.srcs/constrs_1/new/basys3.xdc
+++ 05_elevator_controller/05_elevator_controller.srcs/constrs_1/new/basys3.xdc
@@ -9,22 +9,22 @@ create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports clk
 
 
 ## Switches
-set_property -dict { PACKAGE_PIN V17   IOSTANDARD LVCMOS33 } [get_ports {sw[0]}]
-set_property -dict { PACKAGE_PIN V16   IOSTANDARD LVCMOS33 } [get_ports {sw[1]}]
-set_property -dict { PACKAGE_PIN W16   IOSTANDARD LVCMOS33 } [get_ports {sw[2]}]
-set_property -dict { PACKAGE_PIN W17   IOSTANDARD LVCMOS33 } [get_ports {sw[3]}]
-set_property -dict { PACKAGE_PIN W15   IOSTANDARD LVCMOS33 } [get_ports {sw[4]}]
-set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33 } [get_ports {sw[5]}]
-set_property -dict { PACKAGE_PIN W14   IOSTANDARD LVCMOS33 } [get_ports {sw[6]}]
-set_property -dict { PACKAGE_PIN W13   IOSTANDARD LVCMOS33 } [get_ports {sw[7]}]
-set_property -dict { PACKAGE_PIN V2    IOSTANDARD LVCMOS33 } [get_ports {sw[8]}]
-set_property -dict { PACKAGE_PIN T3    IOSTANDARD LVCMOS33 } [get_ports {sw[9]}]
-set_property -dict { PACKAGE_PIN T2    IOSTANDARD LVCMOS33 } [get_ports {sw[10]}]
-set_property -dict { PACKAGE_PIN R3    IOSTANDARD LVCMOS33 } [get_ports {sw[11]}]
-set_property -dict { PACKAGE_PIN W2    IOSTANDARD LVCMOS33 } [get_ports {sw[12]}]
-set_property -dict { PACKAGE_PIN U1    IOSTANDARD LVCMOS33 } [get_ports {sw[13]}]
-set_property -dict { PACKAGE_PIN T1    IOSTANDARD LVCMOS33 } [get_ports {sw[14]}]
-set_property -dict { PACKAGE_PIN R2    IOSTANDARD LVCMOS33 } [get_ports {sw[15]}]
+set_property -dict { PACKAGE_PIN V17   IOSTANDARD LVCMOS33 } [get_ports {switch[0]}]
+set_property -dict { PACKAGE_PIN V16   IOSTANDARD LVCMOS33 } [get_ports {switch[1]}]
+set_property -dict { PACKAGE_PIN W16   IOSTANDARD LVCMOS33 } [get_ports {switch[2]}]
+set_property -dict { PACKAGE_PIN W17   IOSTANDARD LVCMOS33 } [get_ports {switch[3]}]
+set_property -dict { PACKAGE_PIN W15   IOSTANDARD LVCMOS33 } [get_ports {switch[4]}]
+set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33 } [get_ports {switch[5]}]
+set_property -dict { PACKAGE_PIN W14   IOSTANDARD LVCMOS33 } [get_ports {switch[6]}]
+set_property -dict { PACKAGE_PIN W13   IOSTANDARD LVCMOS33 } [get_ports {switch[7]}]
+set_property -dict { PACKAGE_PIN V2    IOSTANDARD LVCMOS33 } [get_ports {switch[8]}]
+set_property -dict { PACKAGE_PIN T3    IOSTANDARD LVCMOS33 } [get_ports {switch[9]}]
+set_property -dict { PACKAGE_PIN T2    IOSTANDARD LVCMOS33 } [get_ports {switch[10]}]
+set_property -dict { PACKAGE_PIN R3    IOSTANDARD LVCMOS33 } [get_ports {switch[11]}]
+set_property -dict { PACKAGE_PIN W2    IOSTANDARD LVCMOS33 } [get_ports {switch[12]}]
+set_property -dict { PACKAGE_PIN U1    IOSTANDARD LVCMOS33 } [get_ports {switch[13]}]
+set_property -dict { PACKAGE_PIN T1    IOSTANDARD LVCMOS33 } [get_ports {switch[14]}]
+set_property -dict { PACKAGE_PIN R2    IOSTANDARD LVCMOS33 } [get_ports {switch[15]}]
 
 
 ## LEDs
--- 05_elevator_controller/05_elevator_controller.srcs/sources_1/new/elevator_controller.vhd
+++ 05_elevator_controller/05_elevator_controller.srcs/sources_1/new/elevator_controller.vhd
@@ -34,35 +34,34 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity elevator_controller is
   Port ( 
     clk : in std_logic;
-    sw : in std_logic_vector(15 downto 0);
+    switch : in std_logic_vector(15 downto 0);
     led : out std_logic_vector(15 downto 0)
     );
 end elevator_controller;
 
 architecture Behavioral of elevator_controller is
-type elevator_state is (IDLE, MOVING_UP, MOVING_DOWN, DOOR_OPEN_UP, DOOR_OPEN_DOWN);
-signal current_state : elevator_state := IDLE;
-signal next_state: elevator_state := IDLE;
-
-signal elevator_ptr : natural range 0 to 15 := 0;
-
-signal tick : std_logic := '0';
-signal clk_counter : natural := 0;
+type t_elevator_state is (IDLE, MOVING_UP, MOVING_DOWN, DOOR_OPEN_UP, DOOR_OPEN_DOWN);
+constant c_MAX_FLOOR : natural := 15;
+signal s_current_state : t_elevator_state := IDLE;
+signal s_next_state: t_elevator_state := IDLE;
+signal s_elevator_ptr : natural range 0 to c_MAX_FLOOR := 0;
+signal s_tick : std_logic := '0';
+signal s_clk_counter : natural := 0;
 constant c_CLK_DIVIDER : natural := 25000000; --2Hz
 
 begin
 
-fsm : process (current_state, sw, elevator_ptr) is
+p_fsm : process (s_current_state, switch, s_elevator_ptr) is
     procedure check_requests(
-        signal sw_in : in std_logic_vector(15 downto 0);
-        signal ptr_in : in integer range 0 to 15;
+        signal sw_in : in std_logic_vector(c_MAX_FLOOR downto 0);
+        signal ptr_in : in integer range 0 to c_MAX_FLOOR;
         variable req_above : out std_logic;
         variable req_below : out std_logic
     ) is
     begin
         req_above := '0';
         req_below := '0';
-        for i in 0 to 15 loop
+        for i in 0 to c_MAX_FLOOR loop
             if sw_in(i) = '1' then
                 if i > ptr_in then 
                     req_above := '1';
@@ -77,81 +76,81 @@ fsm : process (current_state, sw, elevator_ptr) is
     variable v_below : std_logic;
 
 begin
-    next_state <= current_state;
-    check_requests(sw, elevator_ptr, v_above, v_below);
+    s_next_state <= s_current_state;
+    check_requests(switch, s_elevator_ptr, v_above, v_below);
 
-    case current_state is
+    case s_current_state is
         when IDLE =>
-            if sw(elevator_ptr) = '1' then
-                next_state <= DOOR_OPEN_UP;
+            if switch(s_elevator_ptr) = '1' then
+                s_next_state <= DOOR_OPEN_UP;
             elsif v_above = '1' then
-                next_state <= MOVING_UP;
+                s_next_state <= MOVING_UP;
             elsif v_below = '1' then
-                next_state <= MOVING_DOWN;
+                s_next_state <= MOVING_DOWN;
             end if;
 
         when MOVING_UP =>
-            if sw(elevator_ptr) = '1' then
-                next_state <= DOOR_OPEN_UP;
+            if switch(s_elevator_ptr) = '1' then
+                s_next_state <= DOOR_OPEN_UP;
             elsif v_above = '1' then
-                next_state <= MOVING_UP;
+                s_next_state <= MOVING_UP;
             elsif v_below = '1' then
-                next_state <= MOVING_DOWN;
+                s_next_state <= MOVING_DOWN;
             else
-                next_state <= IDLE;
+                s_next_state <= IDLE;
             end if;
 
         when MOVING_DOWN =>
-            if sw(elevator_ptr) = '1' then
-                next_state <= DOOR_OPEN_DOWN;
+            if switch(s_elevator_ptr) = '1' then
+                s_next_state <= DOOR_OPEN_DOWN;
             elsif v_below = '1' then
-                next_state <= MOVING_DOWN;
+                s_next_state <= MOVING_DOWN;
             elsif v_above = '1' then
-                next_state <= MOVING_UP;
+                s_next_state <= MOVING_UP;
             else
-                next_state <= IDLE;
+                s_next_state <= IDLE;
             end if;
 
         when DOOR_OPEN_UP =>
-            if sw(elevator_ptr) = '1' then
-                next_state <= DOOR_OPEN_UP;
+            if switch(s_elevator_ptr) = '1' then
+                s_next_state <= DOOR_OPEN_UP;
             else
                 if v_above = '1' then
-                    next_state <= MOVING_UP;
+                    s_next_state <= MOVING_UP;
                 elsif v_below = '1' then
-                    next_state <= MOVING_DOWN;
+                    s_next_state <= MOVING_DOWN;
                 else
-                    next_state <= IDLE;
+                    s_next_state <= IDLE;
                 end if;
             end if;
 
         when DOOR_OPEN_DOWN =>
-            if sw(elevator_ptr) = '1' then
-                next_state <= DOOR_OPEN_DOWN;
+            if switch(s_elevator_ptr) = '1' then
+                s_next_state <= DOOR_OPEN_DOWN;
             else
                 if v_below = '1' then
-                    next_state <= MOVING_DOWN;
+                    s_next_state <= MOVING_DOWN;
                 elsif v_above = '1' then
-                    next_state <= MOVING_UP;
+                    s_next_state <= MOVING_UP;
                 else
-                    next_state <= IDLE;
+                    s_next_state <= IDLE;
                 end if;
             end if;
 
         when others =>
-            next_state <= IDLE;
+            s_next_state <= IDLE;
     end case;
-end process;
+end process p_fsm;
 
 p_clock_divider : process (clk) is
 begin
     if rising_edge(clk) then
-        tick <= '0';
-        if clk_counter < c_CLK_DIVIDER then
-            clk_counter <= clk_counter + 1;
+        s_tick <= '0';
+        if s_clk_counter < c_CLK_DIVIDER then
+            s_clk_counter <= s_clk_counter + 1;
         else 
-            clk_counter <= 0;
-            tick <= '1';
+            s_clk_counter <= 0;
+            s_tick <= '1';
         end if;
     end if;
 end process p_clock_divider;
@@ -159,32 +158,33 @@ end process p_clock_divider;
 elevator_update : process(clk) is
 begin
     if rising_edge(clk) then 
-        if tick = '1' then
-            current_state <= next_state;
+        if s_tick = '1' then
+            s_current_state <= s_next_state;
             
-            case next_state  is
+            case s_next_state  is
                 when MOVING_UP =>
-                    if elevator_ptr < 15 then
-                        elevator_ptr <= elevator_ptr + 1;
+                    if s_elevator_ptr < c_MAX_FLOOR then
+                        s_elevator_ptr <= s_elevator_ptr + 1;
                     end if;
                     
                 when MOVING_DOWN =>
-                    if elevator_ptr > 0 then
-                        elevator_ptr <= elevator_ptr - 1;
+                    if s_elevator_ptr > 0 then
+                        s_elevator_ptr <= s_elevator_ptr - 1;
                     end if;
                     
                 when others =>
-                    elevator_ptr <= elevator_ptr; 
+                    s_elevator_ptr <= s_elevator_ptr; 
             end case;
         end if; 
     end if; 
 end process elevator_update;
 
-led_update : process(elevator_ptr) is
+
+p_led_update : process(s_elevator_ptr) is
 begin
 led <= "0000000000000000";
-led(elevator_ptr) <= '1';
-end process led_update;
+led(s_elevator_ptr) <= '1';
+end process p_led_update;
 
 
 end Behavioral;
