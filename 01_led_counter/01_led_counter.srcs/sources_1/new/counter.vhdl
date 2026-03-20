43e4c85c020b3cc58e29b021a73b1852767e7899
kylianchaussoy <kyliannchaussoy@gmail.com>
2026-03-20 17:05:17 -0400

    01 fix clock skew

--- 01_led_counter/01_led_counter.srcs/sources_1/new/counter.vhdl
+++ 01_led_counter/01_led_counter.srcs/sources_1/new/counter.vhdl
@@ -31,59 +31,58 @@ use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity counter is
+entity counter_led is
 generic (
     CLK_DIVISOR1 : natural := 1_000_000;
     CLK_DIVISOR2 : natural := 100_000
 );
-    Port ( clk : in std_logic;
-           switch2 : in std_logic;
-           switch1 : in std_logic;
-           led : out std_logic_vector(15 downto 0)
-           );
-end counter;
+Port ( 
+  clk : in std_logic;
+  switch2 : in std_logic;
+  switch1 : in std_logic;
+  led : out std_logic_vector(15 downto 0)
+);
+end counter_led;
 
-architecture Behavioral of counter is
+architecture Behavioral of counter_led is
     constant c_2POW16 : natural :=  65536;
     signal s_cpt : natural range 0 to c_2POW16 -1 := 0;
-    signal s_clk_divided : std_logic := '0';
     signal s_clk_count : natural := 0;
+    signal s_tick : std_logic := '0'; 
 
 begin
     p_clk_divider : process (clk) is
     variable v_divisor : natural;
     begin
     if rising_edge(clk) then
+        s_tick <= '0';
+        
         if switch2 = '1' then
             v_divisor := CLK_DIVISOR2;
         else v_divisor := CLK_DIVISOR1;
         end if;
 
-        s_clk_count <= s_clk_count +1;
         if s_clk_count >= v_divisor then
-            s_clk_divided <= not s_clk_divided;
+            s_tick <= '1';
             s_clk_count <= 0;
+        else
+            s_clk_count <= s_clk_count + 1;
         end if;
     end if;
     end process p_clk_divider;
 
-    p_increment : process (s_clk_divided) is
+    p_increment : process (clk) is
     begin
-    if rising_edge(s_clk_divided) then
-        if switch1 = '1' then
-            if s_cpt < c_2pow16 - 1 then
-                s_cpt <= s_cpt + 1;
+    if rising_edge(clk) then
+        if s_tick = '1' then
+            if switch1 = '1' then
+                s_cpt <= (s_cpt + 1) mod 65536;
             else s_cpt <= 0;
             end if;
-        else s_cpt <= 0;
         end if;
     end if;
     end process p_increment;
             
-    p_led_update : process (s_cpt) is
-    begin
-        led <= std_logic_vector(to_unsigned(s_cpt, 16));
-    end process p_led_update;
-        
+    led <= std_logic_vector(to_unsigned(s_cpt, 16));
 
 end Behavioral;
